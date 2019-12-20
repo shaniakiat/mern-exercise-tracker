b41d8c6c0854b12ae7d55e73c01a6bb679177625
shaniakiat <kiat.shania@gmail.com>
2019-12-20 15:30:38 -0500

    backend server routes

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("Database Connection Successful!!"))
+  .catch(err => console.error(err));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully");
+});
+
+const usersRouter = require("./routes/users");
+const exercisesRouter = require("./routes/exercises");
+
+app.use("/users", usersRouter);
+app.use("/exercises", exercisesRouter);
+
 // start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
